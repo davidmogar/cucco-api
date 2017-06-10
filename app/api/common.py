48bc779ebf57c3cfe4c7bb3a044e275abb7fc9cc
David Moreno García <david.mogar@gmail.com>
2017-06-11 00:45:20 +0200

    Add common endpoints

---
+++ app/api/common.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, make_response, jsonify, request
+
+from app.common import functions
+from app.common.analytics import track_event
+
+common = Blueprint('common', __name__, url_prefix='/api')
+
+@common.route('/status', methods=['GET'])
+def status():
+    track_event('common', 'status', request.method)
+
+    return make_response(jsonify({ 'status': 'ok' }))
