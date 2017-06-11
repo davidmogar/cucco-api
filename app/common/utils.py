1acfdf651f837047d6645c4095fcc1c6dcd5eb7b
David Moreno García <david.mogar@gmail.com>
2017-06-11 18:46:29 +0200

    Add util functions

---
+++ app/common/utils.py
@@ -0,0 +1,37 @@
+from distutils.util import strtobool
+from flask import make_response, jsonify
+
+def get_data(request, keys):
+    values = []
+
+    data = request.get_json() if request.method == 'POST' else None
+
+    for key in keys:
+        if data:
+            values.append(data[key] if key in data else None)
+        else:
+            values.append(request.values.get(key))
+
+    return values
+
+def prepare_response(result, keys, values):
+    data = dict()
+
+    for i, key in enumerate(keys):
+        if values[i]:
+            data[key] = values[i]
+
+    data['text'] = result
+
+    return make_response(jsonify(data))
+
+def to_boolean(value, none_value=False):
+    if value is None:
+        value = none_value
+    elif not isinstance(value, bool):
+        try:
+            value = strtobool(value)
+        except ValueError:
+            value = none_value
+
+    return value
