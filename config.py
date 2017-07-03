5270c56e2f23a6dc39c5f613fab48d2e2d3261ff
David Moreno García <david.mogar@gmail.com>
2017-07-03 22:47:53 +0200

    Refactor config classes

--- config.py
+++ config.py
@@ -1,40 +1,22 @@
-import os
-
-class Config(object):
+class BaseConfig(object):
     """Parent configuration class."""
-    API_SECRET = os.getenv('API_SECRET')
     CSRF_ENABLED = True
     DEBUG = False
-    LOG_FOLDER = os.getenv('LOG_FOLDER')
-    SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     RATELIMIT_HEADERS_ENABLED = True
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig(BaseConfig):
     """Configurations for Development."""
     DEBUG = True
 
-class TestingConfig(Config):
+class TestingConfig(BaseConfig):
     """Configurations for Testing.
 
     This Testing configuration uses a separate database.
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
     TESTING = True
 
-class StagingConfig(Config):
-    """Configurations for Staging."""
-    DEBUG = True
-
-class ProductionConfig(Config):
+class ProductionConfig(BaseConfig):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
-
-app_config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'staging': StagingConfig,
-    'testing': TestingConfig,
-}
